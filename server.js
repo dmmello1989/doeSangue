17bb08aceb8f45f7d2026f33670b5cc542cebf09
Daniel Mello <dmmello1989@gmail.com>
2020-02-27 16:19:44 -0300

    integração banco de dados

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ server.use(express.static("public"));
 // habilitar body no form
 server.use(express.urlencoded({ extended: true }));
 
+// configurar conexão com banco de dados
+const Pool = require("pg").Pool
+const db = new Pool({
+  user: "postgres",
+  password: "root",
+  host: "localhost",
+  port: 5432,
+  database: "donors"
+})
+
 // template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
@@ -14,27 +24,8 @@ nunjucks.configure("./", {
   noCache: true
 })
 
-// lista de doadores
-const donors = [
-  {
-    name: "Diego Fernandes",
-    blood: "AB+"
-  },
-  {
-    name: "Daniel Mello",
-    blood: "A+"
-  },
-  {
-    name: "Giovanna Santana",
-    blood: "B+"
-  },
-  {
-    name: "Michael Jackson",
-    blood: "O-"
-  }
-];
-
 server.get("/", (req, res) => {
+  const donors = [];
   return res.render("index.html", { donors });
 });
 
@@ -43,12 +34,19 @@ server.post("/", (req, res) => {
   const email = req.body.email;
   const blood = req.body.blood;
 
-  donors.push({ 
-    name: name,
-    blood: blood,
-  });
+  if (name == "" || email == "" || blood == "") {
+    return res.send("Todos os campos são obrigatórios.")
+  }
 
-  return res.redirect("/");
+// colocar valores dentro do banco de dados
+  const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`;
+  const values = [name, email, blood];
+  db.query(query, values, function(err) {
+    if(err) return res.send("Erro no banco de dados.")
+    console.log(err)
+
+    return res.redirect("/");
+  })
 });
 
 server.listen(3000, () => {
